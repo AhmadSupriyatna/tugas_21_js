294ff09ddb9410cf7ca97949066cd8641e48b1e8
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-23 18:41:50 +0700

    Tugas 21 Javascript
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas21.js
@@ -0,0 +1,7 @@
+function duaSatu(){
+    var kal = "Saya belajar masak nasi goreng";
+    console.log(kal);
+    var hasil = kal.replace("masak", "makan");
+    return hasil
+}
+console.log(duaSatu());
